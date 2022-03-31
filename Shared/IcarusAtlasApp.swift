895d5a1c40ba63545ab4f24084f3c04f02fcd1b3
bermudalocket <github@eigenstuff.net>
2022-03-31 17:04:45 -0400

    Initial Commit

---
+++ Shared/IcarusAtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  IcarusAtlasApp.swift
+//  Shared
+//
+//  Created by Michael Rippe on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct IcarusAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
